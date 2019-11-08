ccc929933b42d6b0415d8aa178ee9744766c9430
msarton5 <55455852+msarton5@users.noreply.github.com>
2019-11-07 19:59:34 -0500

    Update months.js

--- src/months.js
+++ src/months.js
@@ -1,9 +1,31 @@
 const readlineSync = require("readline-sync");
 
-const Month = Number(readlineSync.question("\nEnter a month: "));
+const Month = String(readlineSync.question("\nEnter a month: "));
 
-if (Number.isNaN(Amount)) {
-    console.log("Invalid")
-} else if (Month = "jan") {
-    console.log("Invalid")
+if (Month == "jan") {
+    console.log("\n\n31 days.\n")
+} else if (Month == "feb") {
+    console.log("\n28 or 29 days.\n")
+} else if (Month == "mar") {
+    console.log("\n31 days.\n")
+} else if (Month == "apr") {
+    console.log("\n30 days.\n")
+} else if (Month == "may") {
+    console.log("\n31 days.\n")
+} else if (Month == "jun") {
+    console.log("\n30 days.\n")
+} else if (Month == "jul") {
+    console.log("\n31 days.\n")
+} else if (Month == "aug") {
+    console.log("\n31 days.\n")
+} else if (Month == "sep") {
+    console.log("\n30 days.\n")
+} else if (Month == "oct") {
+    console.log("\n31 days.\n")
+} else if (Month == "nov") {
+    console.log("\n30 days.\n")
+} else if (Month == "dec") {
+    console.log("\n31 days.\n")
 } else {
+    console.log("\nInvalid.\n")
+}
