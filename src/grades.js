35aef1dc5203f9cde73dec8b3a79a1a2e8f5c0db
msarton5 <55455852+msarton5@users.noreply.github.com>
2019-11-07 19:53:21 -0500

    added \n

--- src/grades.js
+++ src/grades.js
@@ -6,17 +6,17 @@ const MIN = 0;
 const MAX = 100;
 
 if (Number.isNaN(Grade)) {
-    console.log("\nInvalid.")
+    console.log("\nInvalid.\n")
 } else if (100 >= Grade && Grade >= 90) {
-    console.log("\nYou received an A.")
+    console.log("\nYou received an A.\n")
 } else if (90 > Grade && Grade >= 80) {
-    console.log("\nYou received a B.")
+    console.log("\nYou received a B.\n")
 } else if (80 > Grade && Grade >= 70) {
-    console.log("\nYou received a C.")
+    console.log("\nYou received a C.\n")
 } else if (70 > Grade && Grade >= 60) {
-    console.log("\nYou received a D.")
+    console.log("\nYou received a D.\n")
 } else if (60 > Grade && Grade >= 0) {
-    console.log("\nYou received an F.")
+    console.log("\nYou received an F.\n")
 } else {
-    console.log("\nInvalid.")
+    console.log("\nInvalid.\n")
 }
