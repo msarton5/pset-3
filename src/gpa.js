1495188cf34eacd31bac11de58fc648c41ed31a7
msarton5 <55455852+msarton5@users.noreply.github.com>
2019-11-07 19:57:58 -0500

    Manually updated gpa.js

--- src/gpa.js
+++ src/gpa.js
@@ -1,6 +1,33 @@
 const readlineSync = require("readline-sync");
 
-const Amount = Number(readlineSync.question("\nEnter a number: "));
+const Letter = String(readlineSync.question("\nEnter a letter grade: "));
 
-let Val = Number.MIN_SAFE_INTEGER;
-let Val = Number.MAX_SAFE_INTEGER;
+if (Letter === "A") {
+    console.log("\nYour GPA is 4.00.\n")
+} else if (Letter === "A+") {
+    console.log("\nYour GPA is 4.00.\n")
+} else if (Letter === "A-") {
+    console.log("\nYour GPA is 3.67.\n")
+} else if (Letter === "B+") {
+    console.log("\nYour GPA is 3.33.\n")
+} else if (Letter === "B"){
+    console.log("\nYour GPA is 3.00.\n")
+} else if (Letter === "B-") {
+    console.log("\nYour GPA is 2.67.\n")
+} else if (Letter === "C+") {
+    console.log("\nYour GPA is 2.33.\n")
+} else if (Letter === "C") {
+    console.log("\nYour GPA is 2.00.\n")
+} else if (Letter === "C-") {
+    console.log("\nYour GPA is 1.67.\n")
+} else if (Letter === "D+") {
+    console.log("\nYour GPA is 1.33.\n")
+} else if (Letter === "D") {
+    console.log("\nYour GPA is 1.00.\n")
+} else if (Letter === "D-") {
+    console.log("\nYour GPA is 0.67.\n")
+} else if (Letter === "F") {
+    console.log("\nYour GPA is 0.00.\n")
+} else {
+    console.log("\nInvalid.\n")
+}
